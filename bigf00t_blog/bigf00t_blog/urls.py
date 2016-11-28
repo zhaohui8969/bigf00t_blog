7ab311fc06b39c99c18011614279576df389e25f
zhaohui8969 <natas_hw@163.com>
2016-11-28 19:09:43 +0800

    修复bigf00t_blog/urls.py中单词拼写错误

--- bigf00t_blog/bigf00t_blog/urls.py
+++ bigf00t_blog/bigf00t_blog/urls.py
@@ -25,9 +25,9 @@ urlpatterns = [
     url(r'^posts/', include("blog.urls", namespace='posts')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, documnet_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, documnet_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
